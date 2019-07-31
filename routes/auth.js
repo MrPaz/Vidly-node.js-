c475e05198238b6cdceede6da05efe140b89d4b3
Matt Pazar <mattpazar@yahoo.com>
2019-07-31 10:15:10 -0500

    refactor to encapsulate token gen in user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,8 @@
-const config = require('config');
 const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
-const jwt = require('jsonwebtoken');
 
 router.post('', async (req, res) => {
     const { error } = validate(req.body);
@@ -16,7 +14,7 @@ router.post('', async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+    const token = user.generateAuthToken();
     res.send(token);
 });
 
